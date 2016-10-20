2e658005927d37d273909fe032a1a0ada1b23a6d
jinfa <chuajinfa@gmail.com>
2016-10-20 21:19:34 +0800

    layout updates

--- SIAapp - Copy/app/src/main/java/kenneth/jf/siaapp/thirdFrag.java
+++ SIAapp - Copy/app/src/main/java/kenneth/jf/siaapp/thirdFrag.java
@@ -62,7 +62,7 @@ public class thirdFrag extends Fragment implements BeaconConsumer {
         super.onResume();
         mBeaconManager = BeaconManager.getInstanceForApplication(this.getApplicationContext());
         // Detect the main Eddystone-UID frame:
-        mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
+   mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
         mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("x,s:0-1=feaa,m:2-2=20,d:3-3,d:4-5,d:6-7,d:8-11,d:12-15"));
         mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
         mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20v"));
