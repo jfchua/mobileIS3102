e6f9441a1a7e9ee732080989d54d2610605869d8
jinfa <chuajinfa@gmail.com>
2016-10-23 21:27:07 +0800

    latest kenneth

--- SIAapp - Copy/app/src/main/java/kenneth/jf/siaapp/test.java
+++ SIAapp - Copy/app/src/main/java/kenneth/jf/siaapp/test.java
@@ -1,23 +1,34 @@
 package kenneth.jf.siaapp;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.TableRow.LayoutParams;
 
+import com.google.zxing.BarcodeFormat;
+
+import static android.content.Context.WINDOW_SERVICE;
+
 /**
  * Created by User on 21/10/2016.
  */
 
-public class test extends Fragment {
+public class test extends Fragment implements View.OnClickListener {
     View myView;
     TableLayout tl;
     TableRow tr;
@@ -27,6 +38,13 @@ public class test extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         myView = inflater.inflate(R.layout.test,container,false);  tl = (TableLayout) myView.findViewById(R.id.maintable);
+        qrInput = (TextView) myView.findViewById(R.id.qrInput);
+        dashboard qr = (dashboard)getActivity();
+        String qrcode = qr.getResult();
+        qrInput.setText(qrcode);
+        Button button1 = (Button) myView.findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
         addHeaders();
         addData();
 
@@ -171,4 +189,54 @@ public class test extends Fragment {
                     LayoutParams.WRAP_CONTENT));
         }
     }
+
+
+
+    private String LOG_TAG = "GenerateQRCode";
+
+    TextView qrInput;
+
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.button1:
+
+
+                String qrInputText = qrInput.getText().toString();
+                Log.v(LOG_TAG, qrInputText);
+
+                //Find screen size
+                WindowManager manager = (WindowManager) getActivity().getSystemService(WINDOW_SERVICE);
+                Display display = manager.getDefaultDisplay();
+                Point point = new Point();
+                display.getSize(point);
+                int width = point.x;
+                int height = point.y;
+                int smallerDimension = width < height ? width : height;
+                smallerDimension = smallerDimension * 3/4;
+
+                //Encode with a QR Code image
+                QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(qrInputText,
+                        null,
+                        Contents.Type.TEXT,
+                        BarcodeFormat.QR_CODE.toString(),
+                        smallerDimension);
+                try {
+                    Bitmap bitmap = qrCodeEncoder.encodeAsBitmap();
+                    ImageView myImage = (ImageView) myView.findViewById(R.id.imageView1);
+                    myImage.setImageBitmap(bitmap);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+                break;
+
+            // More buttons go here (if any) ...
+
+        }
+    }
+
 }
