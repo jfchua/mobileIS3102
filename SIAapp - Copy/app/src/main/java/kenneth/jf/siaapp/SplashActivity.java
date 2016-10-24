5870a0d8c1855f44a11faa0d6ae1c6aeeb1082a8
kenneth <a0124975@u.nus.edu>
2016-10-24 11:50:01 +0800

    fixed splashscreen

--- SIAapp - Copy/app/src/main/java/kenneth/jf/siaapp/SplashActivity.java
+++ SIAapp - Copy/app/src/main/java/kenneth/jf/siaapp/SplashActivity.java
@@ -15,7 +15,7 @@ public class SplashActivity extends AppCompatActivity{
         Thread timerThread = new Thread(){
             public void run(){
                 try{
-                    sleep(1500);
+                    sleep(2000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally{
